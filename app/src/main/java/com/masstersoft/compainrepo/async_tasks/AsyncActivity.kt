65122670c0a7c22fcc584ba01cbccd06d782b553
Masster <andrei80586@gmail.com>
2019-08-06 21:37:15 +0300

    AsyncUpdate

--- app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivity.kt
+++ app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivity.kt
@@ -1,13 +1,45 @@
 package com.masstersoft.compainrepo.async_tasks
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.masstersoft.compainrepo.R
+import kotlinx.android.synthetic.main.activity_async.*
 
 class AsyncActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
+
+        btn1.setOnClickListener {
+            @SuppressLint("StaticFieldLeak")
+            object : AsyncTask<Void, Int, String>() {
+                override fun onPreExecute() {
+                    super.onPreExecute()
+                }
+
+                override fun doInBackground(vararg p0: Void?): String {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onPostExecute(result: String?) {
+                    super.onPostExecute(result)
+                }
+
+                override fun onProgressUpdate(vararg values: Int?) {
+                    super.onProgressUpdate(*values)
+                }
+            }
+        }
+
+        btn2.setOnClickListener {
+
+        }
+
+        btn3.setOnClickListener {
+
+        }
     }
 }
