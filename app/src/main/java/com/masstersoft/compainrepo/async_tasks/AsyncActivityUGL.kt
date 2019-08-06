3dd9ba539109c5dfaffc3af4c12ca531063dcb96
Uglyroid <uglyroid@gmail.com>
2019-08-06 21:28:38 +0300

    UGL add Activity

---
+++ app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivityUGL.kt
@@ -0,0 +1,13 @@
+package com.masstersoft.compainrepo.async_tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masstersoft.compainrepo.R
+
+class AsyncActivityUGL : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_async)
+    }
+}
