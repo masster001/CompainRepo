c99c55124268e76f6a3372901493816fe10e0ee5
Masster <andrei80586@gmail.com>
2019-08-12 20:14:47 +0300

    Easy Handler example

--- app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivity.kt
+++ app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivity.kt
@@ -2,16 +2,59 @@ package com.masstersoft.compainrepo.async_tasks
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.masstersoft.compainrepo.R
 import kotlinx.android.synthetic.main.activity_async.*
 
+
 class AsyncActivity : AppCompatActivity() {
 
+    private lateinit var handler: Handler
+    var gameOn: Boolean = false
+    var startTime: Long = 0
+
+    private val TAG = "CompainRepo"
+
+    override fun onPause() {
+        super.onPause()
+        gameOn = false
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameOn = true
+        handler.sendEmptyMessage(0)
+        Log.i(TAG, "onResume")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
+
+        startTime = System.currentTimeMillis()
+        Log.i(TAG, "Before handler=...")
+        handler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                Log.i(TAG, "Start handleMessage")
+                super.handleMessage(msg)
+
+                if (gameOn) {
+                    Log.i(TAG, "GameOn = true")
+                    val seconds = (System.currentTimeMillis() - startTime) / 1000
+                    Log.i(TAG, "seconds = $seconds")
+                    handler.sendEmptyMessageDelayed(0, 1000)
+                    Log.i(TAG, "After sendEmptyMessageDelayed")
+                }else{
+                    Log.i(TAG, "Handler stopped")
+                }
+            }
+        }
 
         btn1.setOnClickListener {
             object : AsyncTask<Double, Int, String>() {
