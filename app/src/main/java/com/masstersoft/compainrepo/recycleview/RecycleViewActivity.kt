40d3e1897c5d1da8e6c853329a16fa61289a4ca3
Masster <andrei80586@gmail.com>
2019-08-13 20:40:41 +0300

    RecycleView Activity added

---
+++ app/src/main/java/com/masstersoft/compainrepo/recycleview/RecycleViewActivity.kt
@@ -0,0 +1,13 @@
+package com.masstersoft.compainrepo.recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masstersoft.compainrepo.R
+
+class RecycleViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycle_view)
+    }
+}
