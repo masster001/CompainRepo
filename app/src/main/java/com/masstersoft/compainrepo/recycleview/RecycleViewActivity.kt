f3d5350c7a87349495261b4ef0cc1cd762848700
Masster <andrei80586@gmail.com>
2019-08-13 21:58:10 +0300

    Last editions about recylerview

--- app/src/main/java/com/masstersoft/compainrepo/recycleview/RecycleViewActivity.kt
+++ app/src/main/java/com/masstersoft/compainrepo/recycleview/RecycleViewActivity.kt
@@ -15,8 +15,16 @@ class RecycleViewActivity : AppCompatActivity() {
         itemList.layoutManager = LinearLayoutManager(this)
         val cats = arrayListOf<Cat>()
 
-        for (i in 0..10)
-            cats.add(Cat("Cat $i",i+1,""))
+        val urls = arrayListOf(
+            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+            "https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2255&q=80",
+            "https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+            "https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+            "https://images.unsplash.com/photo-1489084917528-a57e68a79a1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+        )
+        for (i in 0 until urls.size)
+            cats.add(Cat("Cat ${i + 1}", i + 1, urls[i]))
+
         val adapter = CatsAdapter(cats)
         itemList.adapter = adapter
     }
