e35f82f5ef5bce2bf1102e1761e3f92e76c3860b
Masster <andrei80586@gmail.com>
2019-08-13 21:31:52 +0300

    Last info about cats

--- app/src/main/java/com/masstersoft/compainrepo/MainActivity.kt
+++ app/src/main/java/com/masstersoft/compainrepo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import coil.api.load
 import com.masstersoft.compainrepo.Pavel_Activity.PavelMainActivity
 import com.masstersoft.compainrepo.async_tasks.AsyncActivity
+import com.masstersoft.compainrepo.recycleview.RecycleViewActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         btn3.setOnClickListener {
             startActivity(Intent(this, AsyncActivity::class.java))
+        }
+
+        btn4.setOnClickListener {
+            startActivity(Intent(this,RecycleViewActivity::class.java))
         }
 
         img.load("https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80")
--- app/src/main/java/com/masstersoft/compainrepo/recycleview/RecycleViewActivity.kt
+++ app/src/main/java/com/masstersoft/compainrepo/recycleview/RecycleViewActivity.kt
@@ -1,13 +1,23 @@
 package com.masstersoft.compainrepo.recycleview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.masstersoft.compainrepo.R
+import kotlinx.android.synthetic.main.activity_recycle_view.*
 
 class RecycleViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycle_view)
+
+        itemList.layoutManager = LinearLayoutManager(this)
+        val cats = arrayListOf<Cat>()
+
+        for (i in 0..10)
+            cats.add(Cat("Cat $i",i+1,""))
+        val adapter = CatsAdapter(cats)
+        itemList.adapter = adapter
     }
 }
