6c1f86824a50f73323275d251e0533da0c3e26f2
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-08-19 15:57:04 +0300

    Pavel add handler like AsynkTask

--- app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivityPavel.kt
+++ app/src/main/java/com/masstersoft/compainrepo/async_tasks/AsyncActivityPavel.kt
@@ -9,7 +9,11 @@ import android.os.Message
 import android.util.Log
 import androidx.core.view.isVisible
 import com.masstersoft.compainrepo.R
+import kotlinx.android.synthetic.main.activity_async.*
 import kotlinx.android.synthetic.main.activity_async_pavel.*
+import kotlinx.android.synthetic.main.activity_async_pavel.btn1
+import kotlinx.android.synthetic.main.activity_async_pavel.btn2
+import kotlinx.android.synthetic.main.activity_async_pavel.btn3
 
 class AsyncActivityPavel : AppCompatActivity() {
 
@@ -19,56 +23,69 @@ class AsyncActivityPavel : AppCompatActivity() {
 
     private val TAG = "PavelLog"
 
+    override fun onPause() {
+        super.onPause()
+        gameOn = false
+    }
+
+    fun preExecute() {
+        prProgress.isVisible = true
+        tvInfo.text = ""
+    }
+
+    fun postExecute(result: String?) {
+        prProgress.isVisible = false
+        tvInfo.text = result
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_pavel)
 
+
         startTime = System.currentTimeMillis()
-
-        Log.i(TAG, "Before Handler = ...")
+        Log.i(TAG, "Before handler=...")
         handler = object : Handler() {
+
+            var counter = 0
             override fun handleMessage(msg: Message) {
-                Log.i(TAG, "StartHandleMassege")
                 super.handleMessage(msg)
+                counter++
 
-                if (gameOn) {
-                    Log.i(TAG, "GemeOn = true")
-                    val seconds = (System.currentTimeMillis() - startTime) / 1000
-                    Log.i(TAG, "seconds = $seconds")
+                info.text = counter.toString()
+                Log.i(TAG, "New step counter = $counter")
+
+                if (counter < 100) {
+                    handler.sendEmptyMessageDelayed(0, 20)
+                } else {
+                    postExecute("Handler stopped")
+                    Log.i(TAG, "Handler stopped")
+                    counter = 0
                 }
-
-                handler.sendEmptyMessageDelayed(0, 1000)
-                Log.i(TAG, "Affter sendEmptyMassageDelayed")
-
             }
         }
-        gameOn = true
-        handler.sendEmptyMessage(0)
-        Log.i(TAG, "Affter sendEmptyMassageDelayed")
-
         btn1.setOnClickListener {
             object : AsyncTask<Void, Int, String>() {
                 var internetAvailable: Boolean = false
                 override fun onPreExecute() {
-                    prProgress.isVisible = true
-                    tvInfo.text = ""
+                    preExecute()
                     if (internetAvailable) cancel(internetAvailable)
                 }
 
                 override fun doInBackground(vararg p0: Void?): String {
                     if (!isCancelled) return "AsyncTasc cancelled"
 
-                        for (i in 0..100) {
-                            Thread.sleep(200)
-                            publishProgress(i)
-                        }
+                    for (i in 0..100) {
+                        Thread.sleep(20)
+                        publishProgress(i)
+                    }
 
                     return "AsyncTasc stoped"
                 }
 
                 override fun onPostExecute(result: String?) {
-                    prProgress.isVisible = false
-                    tvInfo.text = result
+                    postExecute(result)
                 }
 
                 override fun onProgressUpdate(vararg values: Int?) {
@@ -78,7 +95,9 @@ class AsyncActivityPavel : AppCompatActivity() {
         }
 
         btn2.setOnClickListener {
-
+            handler.sendEmptyMessage(0)
+            preExecute()
+            Log.i(TAG, "Handler started")
         }
 
         btn3.setOnClickListener {
