ea92ac00a565a0726730f78fe78f8d424cedfb40
Masster <andrei80586@gmail.com>
2019-08-20 20:24:25 +0300

    Recycling viewholders

--- app/src/main/java/com/masstersoft/compainrepo/recycleview/CatViewHolder.kt
+++ app/src/main/java/com/masstersoft/compainrepo/recycleview/CatViewHolder.kt
@@ -1,19 +1,34 @@
 package com.masstersoft.compainrepo.recycleview
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.masstersoft.compainrepo.R
 
-class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class CatViewHolder(itemView: View, index: Int, recyclerView: RecyclerView) : RecyclerView.ViewHolder(itemView) {
     val name = itemView.findViewById<TextView>(R.id.name)
     val age = itemView.findViewById<TextView>(R.id.age)
     val photo = itemView.findViewById<ImageView>(R.id.photo)
+    public val id = index
+    val TAG = "CatAdapter"
+
     init {
-        itemView.setOnClickListener{
-            Toast.makeText(itemView.context, "Item clicked", Toast.LENGTH_SHORT).show()
+        itemView.setOnClickListener {
+            if (recyclerView.adapter is CatsAdapter) {
+                //val adapter: CatsAdapter = recyclerView.adapter as CatsAdapter
+                //val cats = adapter.catsList.toMutableList()
+                //cats.removeAt(itemId.toInt())
+                //recyclerView.adapter = CatsAdapter(cats)
+                Log.d(TAG, "Deleted item with name ${(recyclerView.adapter as CatsAdapter).catsList[itemId.toInt()]}")
+                (recyclerView.adapter as CatsAdapter).catsList.removeAt(itemId.toInt())
+                (recyclerView.adapter as CatsAdapter).notifyDataSetChanged()
+            }
         }
+        Log.d(TAG, "View holder created for ${id}")
     }
+
+
+
 }
